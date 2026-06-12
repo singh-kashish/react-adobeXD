bc45a6b427f8c4454b73a0539e1f6dcdfa1239a2
Kashish Singh <kashish.sonu28@gmail.com>
2026-06-12 16:39:43 +0530

    before checkout

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -21,7 +21,6 @@ export default function Home() {
           <Button variant="purple" fullWidth onClick={() => navigate('/login')}>
             Already Registered? Login
           </Button>
-        
       </div>
     </div>
   );
